debf74e5f857d7e9fbe557cc6b3da7c400a4949d
Kelvin Peña <sniper-lavoz@hotmail.com>
2024-01-11 16:19:28 -0500

    commenting action

--- app/api/register/route.tsx
+++ app/api/register/route.tsx
@@ -12,7 +12,7 @@ export async function POST(req: Request) {
 
   try {
     const data: _user & user_credentials = await req.json()
-    let check_email_in_db = await prisma.userCredentials.findMany({where: {OR:[{email:data.email},{username:data.username}]}});
+    // let check_email_in_db = await prisma.userCredentials.findMany({where: {OR:[{email:data.email},{username:data.username}]}});
     // if(check_email_in_db.length > 0) return Response.json({message:"Email o Username already exist"},{status:409});
     // let {  name, lastname, username,age,street ,state ,city ,zipcode , password, email } = data
     // let hash = await argon.hash(password);
