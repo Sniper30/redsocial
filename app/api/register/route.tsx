7a0b9e6ae67dcda87f8428edb554f60134275e10
Kelvin Peña <sniper-lavoz@hotmail.com>
2024-01-11 16:23:30 -0500

    commenting action

--- app/api/register/route.tsx
+++ app/api/register/route.tsx
@@ -12,16 +12,16 @@ export async function POST(req: Request) {
 
   try {
     const data: _user & user_credentials = await req.json()
-    // let check_email_in_db = await prisma.userCredentials.findMany({where: {OR:[{email:data.email},{username:data.username}]}});
-    // if(check_email_in_db.length > 0) return Response.json({message:"Email o Username already exist"},{status:409});
-    // let {  name, lastname, username,age,street ,state ,city ,zipcode , password, email } = data
-    // let hash = await argon.hash(password);
+    let check_email_in_db = await prisma.userCredentials.findMany({where: {OR:[{email:data.email},{username:data.username}]}});
+    if(check_email_in_db.length > 0) return Response.json({message:"Email o Username already exist"},{status:409});
+    let {  name, lastname, username,age,street ,state ,city ,zipcode , password, email } = data
+    let hash = await argon.hash(password);
 
-    // let user = await prisma.user.create({data: {name,lastname,age,address:{street,state,city,zip:zipcode},profilePhoto:'/static/ben-sweet-2LowviVHZ-E-unsplash.jpg',subscription:{create:{}}}})
-    // let credentialsCreated = await prisma.userCredentials.create({data:{user:{connect: {id: user.id}}, email,hash,username,tokenRt:""}});
+    let user = await prisma.user.create({data: {name,lastname,age,address:{street,state,city,zip:zipcode},profilePhoto:'/static/ben-sweet-2LowviVHZ-E-unsplash.jpg',subscription:{create:{}}}})
+    let credentialsCreated = await prisma.userCredentials.create({data:{user:{connect: {id: user.id}}, email,hash,username,tokenRt:""}});
     prisma.$disconnect();
-    // credentialsCreated.hash = '';
-    // return Response.json({ ...user, ...credentialsCreated })
+    credentialsCreated.hash = '';
+    return Response.json({ ...user, ...credentialsCreated })
     return Response.json({ message: "klk" })
   } catch (error) {
     console.log(error);
